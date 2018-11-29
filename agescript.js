1d527f6af13c168dac9c599e5f02b07f3544741e
Natasja Geigert <natasjageigert@Natasjas-MacBook-Pro.local>
2018-11-29 17:59:31 +0100

    updated

--- agescript.js
+++
@@ -1,3 +0,0 @@
-var age = prompt("What us your age?");
-var days = age * 365.25;
-alert(age + " years is roughly " + days + " days");
--- script.js
+++
@@ -1,8 +0,0 @@
-var firstname = prompt("What is your first name?");
-var lastname = prompt("What is your last name?");
-var age = prompt("What is your age?");
-
-var fullname = firstname + " " + lastname
-
-console.log("Your full name is " + fullname);
-console.log("You are " + age + " years old");
